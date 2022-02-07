51ef98962347ef9f3154ffffa767a433b3072c09
Jingting <xingjingting1217@gmail.com>
2022-02-06 16:23:39 -0800

    A3 Clicky version4: add clicky button and modify layout to satisfy rotated requirement

--- app/src/main/java/edu/neu/numad22sp_jingtingxing/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jingtingxing/MainActivity.java
@@ -2,6 +2,7 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,55 +27,14 @@ import org.w3c.dom.Text;
         });
 
         /* A3 Clicky */
-        TextView textContent = (TextView) findViewById(R.id.textView1);
-        textContent.setText("Pressed: -");
-
-        final Button btnA = (Button)findViewById(R.id.buttonA);
-        btnA.setOnClickListener(new View.OnClickListener() {
+        Button btnClicky = (Button) findViewById(R.id.buttonClicky);
+        btnClicky.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textContent.setText("Pressed: A");
+                Intent intent = new Intent(MainActivity.this, clicky.class);
+                startActivity(intent);
             }
         });
 
-        final Button btnB = (Button)findViewById(R.id.buttonB);
-        btnB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textContent.setText("Pressed: B");
-            }
-        });
-
-        final Button btnC = (Button)findViewById(R.id.buttonC);
-        btnC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textContent.setText("Pressed: C");
-            }
-        });
-
-        final Button btnD = (Button)findViewById(R.id.buttonD);
-        btnD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textContent.setText("Pressed: D");
-            }
-        });
-
-        final Button btnE = (Button)findViewById(R.id.buttonE);
-        btnE.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textContent.setText("Pressed: E");
-            }
-        });
-
-        final Button btnF = (Button)findViewById(R.id.buttonF);
-        btnF.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textContent.setText("Pressed: F");
-            }
-        });
     }
 }
---
+++ app/src/main/java/edu/neu/numad22sp_jingtingxing/clicky.java
@@ -0,0 +1,69 @@
+package edu.neu.numad22sp_jingtingxing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class clicky extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky);
+
+        TextView textContent = (TextView) findViewById(R.id.textView1);
+        textContent.setText("Pressed: -");
+
+        final Button btnA = (Button)findViewById(R.id.buttonA);
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textContent.setText("Pressed: A");
+            }
+        });
+
+        final Button btnB = (Button)findViewById(R.id.buttonB);
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textContent.setText("Pressed: B");
+            }
+        });
+
+        final Button btnC = (Button)findViewById(R.id.buttonC);
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textContent.setText("Pressed: C");
+            }
+        });
+
+        final Button btnD = (Button)findViewById(R.id.buttonD);
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textContent.setText("Pressed: D");
+            }
+        });
+
+        final Button btnE = (Button)findViewById(R.id.buttonE);
+        btnE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textContent.setText("Pressed: E");
+            }
+        });
+
+        final Button btnF = (Button)findViewById(R.id.buttonF);
+        btnF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textContent.setText("Pressed: F");
+            }
+        });
+
+    }
+}
